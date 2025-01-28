90bf0d57e28bd43f78b136b4980c90063f2e9092
shaikhskp <sulaiman.husseni@gmail.com>
2025-01-28 11:11:13 -0500

    updated keybinds

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -77,7 +77,7 @@ keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
 
 -- Glow --
 -- Open markdown preview_config
-keymap("n", "<leader>m", ":Glow<CR>", opts)
+--[[ keymap("n", "<leader>m", ":Glow<CR>", opts) ]]
 
 -- Terminal --
 -- Better terminal navigation
@@ -85,4 +85,28 @@ keymap("n", "<leader>m", ":Glow<CR>", opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Magma --
+-- compile code in JupyterNotebooks 
+-- Normal mode, line evaluation
+keymap("n", "<LocalLeader>ml", ":MagmaEvaluateLine<CR>", opts)
+
+-- Visual mode, evaluate selection
+keymap("x", "<LocalLeader>mv", ":<C-u>MagmaEvaluateVisual<CR>", opts)
+
+-- Reevaluate cell
+keymap("n", "<LocalLeader>mc", ":MagmaReevaluateCell<CR>", opts)
+
+-- Delete
+keymap("n", "<LocalLeader>md", ":MagmaDelete<CR>", opts)
+
+-- Show Output
+keymap("n", "<LocalLeader>mo", ":MagmaShowOutput<CR>", opts)
+
+-- Enter Output
+keymap("n", "<LocalLeader>mq", ":noautocmd MagmaEnterOutput<CR>", opts)
+
+vim.cmd [[
+  nmap <expr> <LocalLeader>m magma#evaluate_operator()
+]]
 
